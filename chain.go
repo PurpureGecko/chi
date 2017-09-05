fe8774105863bc9c72f0c1831ad25f32f9b6f1db
Gruchik Vladislav <gruchikvlad@gmail.com>
2017-09-05 12:51:24 +0300

    code improvement in chain

--- chain.go
+++ chain.go
@@ -34,16 +34,9 @@ func (c *ChainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // chain builds a http.Handler composed of an inline middleware stack and endpoint
 // handler in the order they are passed.
 func chain(middlewares []func(http.Handler) http.Handler, endpoint http.Handler) http.Handler {
-	// Return ahead of time if there aren't any middlewares for the chain
-	if len(middlewares) == 0 {
-		return endpoint
-	}
-
 	// Wrap the end handler with the middleware chain
-	h := middlewares[len(middlewares)-1](endpoint)
-	for i := len(middlewares) - 2; i >= 0; i-- {
-		h = middlewares[i](h)
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		endpoint = middlewares[i](endpoint)
 	}
-
-	return h
+	return endpoint
 }
